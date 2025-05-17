6170ce0bd1c02793771da20a5910871498d70047
yastrab1 <lipka.luki@gmail.com>
2025-05-17 18:44:01 +0200

    agent.ts

--- ai-agent-visualization/lib/engine/agent.ts
+++ ai-agent-visualization/lib/engine/agent.ts
@@ -2,13 +2,22 @@ import { TaskGeneralEvent, ExecutionState, TaskID } from "../types"
 import { AsyncQueue } from "./asyncQueue"
 import { generatePlan } from "./planner"
 
-function findIndependentTasks(id: TaskID): TaskID[] {
-    return [];
+export function findIndependentSubTasks(taskID:TaskID, state:ExecutionState) {
+    const result:TaskID[] = []
+
+    for (const subtaskID of state.tasks[taskID].planSubtasks) {
+        if (state.tasks[subtaskID].dependencies.every(task=>state.tasks[task].status === "completed")) {
+            result.push(subtaskID);
+        }
+    }
+
+    return result;
 }
 
-export function runAgent(id: TaskID, resultQueue: AsyncQueue<TaskGeneralEvent>, state: ExecutionState) {
-    const plan = generatePlan(id, resultQueue, state);
-    const independentTasks = findIndependentTasks(id);
+export async function runAgent(id: TaskID, resultQueue: AsyncQueue<TaskGeneralEvent>, state: ExecutionState) {
+    await generatePlan(id, resultQueue, state);
+    const independentTasks = findIndependentSubTasks(id,state);
+    console.log("tasks" + independentTasks);
     for (const task of independentTasks) {
         // executeTask(task, state, resultQueue);
     }
