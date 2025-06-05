ca04a7c9c2e4a715323c42c2ab496833142f6185
yastrab1 <lipka.luki@gmail.com>
2025-06-05 15:16:58 +0200

    package.json module

--- cortx/api/terminal/route.ts
+++ cortx/api/terminal/route.ts
@@ -1,4 +1,4 @@
-export function GET(request: Request) {
+export async function GET(request: Request) {
     const response = await fetch('https://api.vercel.app/products');
     const products = await response.json();
     return Response.json(products);
