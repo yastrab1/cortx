195a9c4916969774b9ad2fcba1b8db9513af0a9e
yastrab1 <lipka.luki@gmail.com>
2025-06-05 15:36:47 +0200

    basic implementation of fluid compute command polling

--- cortx/api/terminal/route.ts
+++ cortx/api/terminal/route.ts
@@ -1,5 +1,48 @@
-export async function GET(request: Request) {
-    const response = await fetch('https://api.vercel.app/products');
-    const products = await response.json();
-    return Response.json(products);
+import {fetchInternalImage} from "next/dist/server/image-optimizer";
+
+interface callCommandBody{
+    command: string;
+    apiKey: string;
+    sessionID:string;
+}
+
+interface checkCommandType{
+    completed:boolean;
+    result:string;
+}
+export async function POST(request: Request) {
+    const awsFuncURL = process.env.AWS_URL;
+
+    if (request.method != "POST"){
+        return Response.error()
+    }
+    const {command, apiKey, sessionID} = await request.json() as callCommandBody;
+
+    const createInstanceQuery = `${awsFuncURL}/createInstance?secretKey=${apiKey}&sessionID=${sessionID}`;
+    const instanceResponse = await fetch(createInstanceQuery)
+    if (!instanceResponse.ok){
+        return Response.error()
+    }
+
+    const initiateCommandQuery = `${awsFuncURL}/initiateCommand?secretKey=${apiKey}&sessionID=${sessionID}`;
+    const initiateResponse = await fetch(initiateCommandQuery)
+    if (!initiateResponse.ok){
+        return Response.error()
+    }
+
+    const {instanceID,commandID} = await initiateResponse.json() as {instanceID:string,commandID:string};
+
+    const checkCommandQuery = `${awsFuncURL}/checkCommand?secretKey=${apiKey}&sessionID=${sessionID}&instanceID=${instanceID}&commandID=${commandID}`;
+    setInterval(async ()=>{
+        const response = await fetch(checkCommandQuery)
+        if (!response.ok){
+            return Response.error()
+        }
+        const body = await response.json() as checkCommandType;
+        if (body.completed) {
+            return Response.json({result:body.result});
+        }
+    },1000)
+
+    return Response.json({});
 }
