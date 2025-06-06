d5ba95dfa8e0ff05f6717a1c4ea50e99e85341eb
yastrab1 <lipka.luki@gmail.com>
2025-06-06 12:59:01 +0200

    improved debug experience

--- cortx/api/terminal/route.ts
+++ cortx/api/terminal/route.ts
@@ -20,8 +20,9 @@ export async function POST(request: Request) {
 
     const createInstanceQuery = `${awsFuncURL}/createInstance?secretKey=${apiKey}&sessionID=${sessionID}`;
     const instanceResponse = await fetch(createInstanceQuery)
+    const instanceResponseJson = await instanceResponse.json()
     if (!instanceResponse.ok){
-        return Response.json("Failed to create instance")
+        return Response.json("Failed to create instance" +instanceResponseJson)
     }
 
     const initiateCommandQuery = `${awsFuncURL}/initiateCommand?secretKey=${apiKey}&sessionID=${sessionID}`;
@@ -31,8 +32,9 @@ export async function POST(request: Request) {
             command:command
         })
     })
+    const initiateResponseJson = await initiateResponse.json()
     if (!initiateResponse.ok){
-        return Response.json("Failed to initiate query")
+        return Response.json("Failed to initiate query" +initiateResponseJson)
     }
 
     const {instanceID,commandID} = await initiateResponse.json() as {instanceID:string,commandID:string};
@@ -40,10 +42,11 @@ export async function POST(request: Request) {
     const checkCommandQuery = `${awsFuncURL}/checkCommand?secretKey=${apiKey}&sessionID=${sessionID}&instanceID=${instanceID}&commandID=${commandID}`;
     setInterval(async ()=>{
         const response = await fetch(checkCommandQuery)
-        if (!response.ok){
-            return Response.json("Failed to check command")
-        }
+
         const body = await response.json() as checkCommandType;
+        if (!response.ok){
+            return Response.json("Failed to check command" +body)
+        }
         if (body.completed) {
             return Response.json({result:body.result});
         }
